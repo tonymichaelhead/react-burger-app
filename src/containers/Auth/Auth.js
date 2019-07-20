0a75b63ebef8ea259cd1bd3a7babdd21c9954ffd
tonymichaelhead <tonymichaelhead@gmail.com>
2019-07-20 11:49:41 -0700

    Add action creators for auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
 import classes from './Auth.css'
+import * as actions from '../../store/actions'
 
 class Auth extends Component {
     state = {
@@ -68,6 +70,11 @@ class Auth extends Component {
         }
         this.setState({controls: updatedControls})
     }
+
+    submitHandler = (event) => {
+        event.preventDefault()
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
+    }
     
     render() {
         const formElementsArray = []
@@ -91,8 +98,8 @@ class Auth extends Component {
         ))
 
         return (
-            <div>
-                <form className={classes.Auth}>
+            <div className={classes.Auth}>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">Submit</Button>
                 </form>
@@ -101,4 +108,10 @@ class Auth extends Component {
     }
 }
 
-export default Auth
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Auth)
